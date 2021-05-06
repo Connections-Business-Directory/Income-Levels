3b5c45128712b45efebaf5a0762d934ef3a077fa
Steven A. Zahm <shazahm1@hotmail.com>
2021-05-06 13:49:14 -0400

    Change link from `http` to `https`.

--- connections_income_levels.php
+++ connections_income_levels.php
@@ -17,7 +17,7 @@
  * Author:            Steven A. Zahm
  * Author URI:        https://connections-pro.com
  * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       connections_income_levels
  * Domain Path:       /languages
  */
